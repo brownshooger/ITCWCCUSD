675d9493ba0863268dfa0c4f16c207b7f4f2dfa9
brownshooger <james.e.contreras@gmail.com>
2020-04-20 16:56:47 -0700

    Copying stuff over to Github Folder

---
+++ Scripts/PowerShell/Copying/copyFiles.ps1
@@ -0,0 +1,24 @@
+Set-Location "$PSScriptRoot"
+$computers = Get-Content .\computers.txt
+$source = "C:\Files\Test\*"
+$dest = "c$\Users\Public\Desktop\Utilities\SuperF4"
+
+
+foreach ($Computer in $Computers) {
+
+    try {
+
+        ((Test-Connection $Computer -Count 3 -Quiet) -eq $true)
+        Write-Host "$Computer is online" -ForegroundColor Green
+        New-Item -ItemType directory -Path "\\$Computer\$dest"
+        Copy-Item -Force $source -Destination "\\$computer\$dest" -Recurse
+
+        }
+
+    catch {
+
+        Write-Host "$Computer is offline" -ForegroundColor Red
+        Add-Content -value $computer -path ".\deadPCs.txt"
+
+        }
+}
