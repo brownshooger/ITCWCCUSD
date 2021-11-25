056c2370942e80fbccdd1af14d2564a9a1c16119
raijutech <35829989+raijutech@users.noreply.github.com>
2021-11-25 13:04:26 -0800

    test

--- PowerShell/AD/Machines that will have trust issues.ps1
+++
@@ -1,3 +0,0 @@
-$logonDate = (Get-Date).AddDays(-180)
-
-Get-ADComputer -Filter { (Enabled -eq $true) -and (PasswordLastSet -lt $logonDate ) } -Properties Name, OperatingSystemVersion, PasswordLastSet | Select-Object Name, OperatingSystemVersion, PasswordLastSet | Sort-Object PasswordLastSet -Descending
